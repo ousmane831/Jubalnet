4b6b5b2d93dba123b73240178b85611d1a3c1f1d
odioud <dioufousmaneniakhar23@gmail.com>
2025-11-14 21:03:18 +0100

    quelques modification

--- backend/crime_report/settings.py
+++ backend/crime_report/settings.py
@@ -147,6 +147,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://10.121.221.230:5173",
+    
 ]
 
 CORS_ALLOW_CREDENTIALS = True
