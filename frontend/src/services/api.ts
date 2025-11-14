a6d1396bf96fd60162fa0b0cf2ff07630509568e
odioud <dioufousmaneniakhar23@gmail.com>
2025-11-14 20:47:12 +0100

    quelques modification

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,4 +1,6 @@
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'http://10.121.221.230:8001/api';
+
+
 
 // ---------------- Types ---------------- //
 export interface ApiResponse<T> {
